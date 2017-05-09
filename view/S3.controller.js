15cafa099261fbbe82800939603b376595e06f9f
mchoquart <mchoquart@censio.fr>
2017-05-09 08:11:19 +0000

    .

--- view/S3.controller.js
+++ view/S3.controller.js
@@ -1056,6 +1056,7 @@ sap.ca.scfld.md.controller.BaseFullscreenController.extend("cfr.etsapp.manage.vi
 			}
 		}
 	},
+	
 	updateData: function() {
 		var n;
 		var m = this.oApplication.getModel("TSM_WEEKLY");
